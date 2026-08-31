f97bbff55ab83fe38324a33ceb4752edd434cdf1
Mideva-Alma <aileenmideva@email.com>
2026-08-31 13:44:16 +0300

    add exercise and workout models

--- server/models.py
+++ server/models.py
@@ -3,3 +3,20 @@ from sqlalchemy.orm import validates
 
 
 db = SQLAlchemy()
+
+
+class Exercise(db.Model):
+    __tablename__ = "exercises"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    category = db.Column(db.String, nullable=False)
+    equipment_needed = db.Column(db.Boolean, nullable=False)
+
+class Workout(db.Model):
+    __tablename__ = "workouts"
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date, nullable=False)
+    duration_minutes = db.Column(db.Integer, nullable=False)
+    notes = db.Column(db.Text)
